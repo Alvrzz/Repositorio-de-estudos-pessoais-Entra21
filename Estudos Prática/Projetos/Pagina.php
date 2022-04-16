60583ba1dcb197d1d0a5eb6fcbd0fbef88379662
perkles <alvarezluan.ti@gmail.com>
2022-04-16 19:44:20 -0300

    primeiro contato com php

---
+++ Estudos Prática/Projetos/Pagina.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Meu primeiro Site em PHP</title>
+</head>
+
+<body>
+
+<?php 
+   for ($i = 0 ; $i < 10 ; $i++ ) 
+     { print ( "linha número" . $i . "<br />" );}
+?>
+
+</body>
+
+</html>
+
+ 
