8d33136a72cce9325be806e851302ec9893a744b
perkles <alvarezluan.ti@gmail.com>
2022-04-25 13:33:22 -0300

    Estudos com python

---
+++ Estudos Prática/Python/Calculadora de desconto.py
@@ -0,0 +1,7 @@
+n = float(input('Digite o numero do produto: '))
+d = float(input('Desconto: '))
+val = (n*d)/100
+se = n - val
+print('---'*30)
+print(f'Valor original R${n:.2f} \nCom desconto de {d}% \nDesconto: R${val:.2f}\nValor com desconto: R${se:.2f} ')
+print('---'*30)
--- Estudos Prática/Python/Moeda.py
+++ Estudos Prática/Python/Moeda.py
@@ -1,9 +1,2 @@
-salario = input ('Salario Desejado?')
-nome = input ('Seu nome')
-profissao = input('profissao')
-hobby = input ('Hobby')
-faculdade = input ('Facul')
-
-print('-'*30)
-print(nome,profissao,salario,faculdade,hobby)
-print('-'*30)
+nome = input('Seu nome: ')
+print ('olá', nome, 'seja bem vindo')
