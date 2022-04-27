5571ee72f8f096b719b132e6a51908a06925ca50
Luan Alvarez <alvarezluan.ti@gmail.com>
2022-04-27 20:08:50 -0300

    teste

--- Estudos Prática/Python/estudos 3.py
+++ Estudos Prática/Python/estudos 3.py
@@ -1,11 +1,22 @@
-import pygame
-
-# Inicializando o mixer PyGame
-pygame.mixer.init()
-
-# Iniciando o Pygame
-pygame.init()
-
-pygame.mixer.music.load('ex021.mp3')
-pygame.mixer.music.play(loops=0, start=0.0)
-pygame.event.wait()
+from time import sleep
+nome = str(input('Digite seu nome: '))
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+n3 = float(input('Terceira nota: '))
+n4 = float(input('Quarta nota: '))
+media = (n1+n2+n3+n4)/4 
+sleep(1)
+print('--'*30)
+print('Vamos ver se você passou de ano...')
+print('--'*30)
+sleep(1)
+if media >=6.0:
+    sleep(1)
+    print(f'Parábens, {nome} você passou de ano! \nSua média foi: {media:.1f}!')
+    print('--'*30)
+else:
+    sleep(1)
+    print(f'Infelizmente, {nome} você não passou de ano...\nSua média foi: {media:.1f}\nBURROKK')
+    print('--'*30)
+print('Fim do programa.')
+print('--'*30)
