9a473ce99fd1b3ce7152f2e4368c02f18caab8ec
Luan Alvarez <alvarezluan.ti@gmail.com>
2022-04-27 21:03:43 -0300

    alterações

--- Estudos Prática/Python/estudos2.1.py
+++ Estudos Prática/Python/estudos2.1.py
@@ -1,9 +1,10 @@
-from random import choice
+from random import shuffle
 j1 = str(input('Digite primeiro nome: '))
 j2 = str(input('Digite segundo nome: '))
 jogador = str(input('Digite terceiro nome: '))
 j4 = str(input('Digite quarto nome: '))
-lista = j1,j2,jogador,j4
+lista = [j1,j2,jogador,j4]
+shuffle(lista)
 print('=-'*30)
-(print(choice(lista)))
+(print(lista))
 print('=-'*30)
