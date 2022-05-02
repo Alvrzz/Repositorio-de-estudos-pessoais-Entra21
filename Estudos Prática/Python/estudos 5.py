2a9aea83c2ae2b21f7cb779c83da7c274d619ae1
Luan Alvarez <alvarezluan.ti@gmail.com>
2022-05-02 20:03:58 -0300

    estudos python

---
+++ Estudos Prática/Python/estudos 5.py
@@ -0,0 +1,9 @@
+a = int(input('Digite um número aqui: '))
+b = int(input('Digite outro número aqui: '))
+
+if a > b:
+    print(f'{a} é maior que {b}')
+elif b > a:
+    print(f'{b} é maior que {a}')
+else:
+    print(f'{a} e {b} são íguais...')
