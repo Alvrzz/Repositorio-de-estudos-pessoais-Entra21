6953a7d88a8c89325b5b5034d71f6a95085510d0
Luan Alvarez <alvarezluan.ti@gmail.com>
2022-05-10 20:02:08 -0300

    Portugol

---
+++ Entra 21/Exercícios/exercicio_004c.portugol.funcao.sh
@@ -0,0 +1,12 @@
+
+programa(){
+    primeira_funcao(){
+
+        echo "Entrou:$1"
+    }
+    inicio(){
+        primeira_funcao "mensagem de texto enviada pra funcao"
+    }
+    inicio
+}
+programa
