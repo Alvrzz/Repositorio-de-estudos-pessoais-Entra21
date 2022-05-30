d5bf72a595cdbd0f1abe2c43be45fa0533f2fd1f
Luan Alvarez <alvarezluan.ti@gmail.com>
2022-05-29 23:54:46 -0300

    Update JOKENPO.py

--- Estudos Prática/Python/JOKENPO.py
+++ Estudos Prática/Python/JOKENPO.py
@@ -1,48 +1,58 @@
-from random import randint
+from random import randint 
 from time import sleep
-itens = 'Pedra','Papel','Tesoura'
+itens = 'Pedra','Papel','Tesoura' 
 pc = randint(0,2)
-computador = 'O computador venceu com'
+computador = 'O computador venceu!!\nCom:'
 jogador = 'O jogador ganhou!!\nCom:'
-print('--'*31)
+print('=-'*31)
 player = (input(
-'''JOKENPO
-[ 0 ]Pedra
-[ 1 ]Papel
-[ 2 ]Tesoura
-Sua escolha: '''))
+'''     JOKENPO
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura
+Sua escolha: ''')).strip()
+
 print('-='*31) 
-if player:
-    player = int(player)
-    print('JO')
-    sleep(1)
-    print('KEN')
-    sleep(1)
-    print('PO!')
-    sleep(1)
-    print('-='*30)
-    if pc == player or player == pc: #empate
-        print(f'Empatou!!!\nComputador jogou: {itens[pc]}\nE você jogou: {itens[player]}!')
 
-    elif pc == 0: #computador jogou pedra
-        if player == 1:
-            print(f'{jogador} {itens[player]}!\nEnquanto o computador jogou: {itens[pc]}!')
-        elif player == 2:
-            print(f'{computador} {itens[pc]}!!')
+try:
+    if player == '0' or  player =='1' or  player == '2':
+        player = int(player)
+        print(f'Você escolheu {itens[player]}!')
+        sleep(1)
+        print('E vamos ao jogo!!!\n')
+        sleep(2)
+        print('JO..\n')
+        sleep(1)
+        print('KEN..\n')
+        sleep(1)
+        print('PO!\n')
+        sleep(1)
+        print('-='*30)
+        if pc == player or player == pc: #empate
+            print(f'Empatou!!!\nComputador jogou: {itens[pc]}\nE você jogou: {itens[player]}!')
 
-    elif pc == 1: #computador jogou papel
-        if player == 0:
-            print(f'{computador} {itens[pc]}!! ')
-        elif player == 2:
-            print(f'{jogador} {itens[player]}!\nEnquanto o computador jogou: {itens[pc]}!')
+        elif pc == 0: 
+            if player == 1:
+                print(f'{jogador} {itens[player]}!\nEnquanto o computador jogou: {itens[pc]}!')
+            elif player == 2:
+                print(f'{computador} {itens[pc]}!\nEnquanto o jogador jogou: {itens[player]}!')
 
-    elif pc == 2: #computador jogou tesoura~
-        if player == 0:
-            print(f'{jogador} {itens[player]}!\nEnquanto o computador jogou: {itens[pc]}!')
-        elif player == 1:
-            print(f'{computador} {itens[pc]}')
-else:   
-    print('Digite corretamente sua opção.')    
+        elif pc == 1:
+            if player == 0:
+                print(f'{computador} {itens[pc]}!\nEnquanto o jogador jogou: {itens[player]} ')
+            elif player == 2:
+                print(f'{jogador} {itens[player]}!\nEnquanto o computador jogou: {itens[pc]}!')
+
+        elif pc == 2: 
+            if player == 0:
+                print(f'{jogador} {itens[player]}!\nEnquanto o computador jogou: {itens[pc]}!')
+            elif player == 1:
+                print(f'{computador} {itens[pc]}!\nEnquanto o jogador jogou: {itens[player]}!')
+    else:
+        print('Digite corretamente sua opção.')                
+except:
+    print('Digite corretamente sua opção.')
+
 
 print('-='*31)
 
